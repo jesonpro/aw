079ed9f5f664a4a79bdb4f21a621589e7810aa50
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-29 01:24:32 +0530

    Create Tiktok.js

---
+++ plugins/Tiktok.js
@@ -0,0 +1,103 @@
+const Asena = require('../events');
+const {MessageType,mimetype,Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('scrapers');
+const ALang = Language.getString('amazone');
+const capt = "🦹‍♀️Amazone Tiktok Downloader* 🕊\n"
+
+if (Config.WORKTYPE == 'public') {
+	
+Asena.addCommand({ pattern: 'ttv ?(.*)', fromMe: false, desc: ALang.TIKTOk }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Tiktok Link* ")
+
+    await message.sendMessage('👻 *Tiktok Video Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
+
+				await axios
+					.get(`https://zenzapi.xyz/api/downloader/tiktok?url=${link}&apikey=5c3ceca04a58`)
+					.then(async(response) => {
+						const {
+							nowatermark
+						} = response.data.result
+
+						const linkdata = await axios.get(nowatermark, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: capt,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID TIKTOK LINK*"),
+							)
+					})
+					
+}) //pattern close
+  
+  Asena.addCommand({ pattern: 'ttv ?(.*)', fromMe: true, desc: ALang.TIKTOk }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Tiktok Link* ")
+
+    await message.sendMessage('👻 *Tiktok Video Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
+
+				await axios
+					.get(`https://zenzapi.xyz/api/downloader/tiktok?url=${link}&apikey=5c3ceca04a58`)
+					.then(async(response) => {
+						const {
+							nowatermark
+						} = response.data.result
+
+						const linkdata = await axios.get(nowatermark, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: capt,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID TIKTOK LINK*"),
+							)
+					})
+					
+}) //pattern close
+  
+} /* public close*/
+
+else if (Config.WORKTYPE == 'private') {
+  
+  Asena.addCommand({ pattern: 'ttv ?(.*)', fromMe: true, dontaddCommandList:true }, async (message, match) => {
+
+    const link = match[1]
+
+    if (!link) return await message.sendMessage(" *Give Vaild Tiktok Link* ")
+
+    await message.sendMessage('👻 *Tiktok Video Downloader* 🕊 \n'+Lang.DOWNLOADING_VIDEO)
+
+				await axios
+					.get(`https://zenzapi.xyz/api/downloader/tiktok?url=${link}&apikey=5c3ceca04a58`)
+					.then(async(response) => {
+						const {
+							nowatermark
+						} = response.data.result
+
+						const linkdata = await axios.get(nowatermark, {
+							responseType: 'arraybuffer'
+						})
+
+						await message.sendMessage(Buffer.from(linkdata.data), MessageType.video, {
+								caption: capt,
+						})
+							.catch(
+								async(err) => await message.sendMessage("⛔️ *INVALID TIKTOK LINK*"),
+							)
+					})
+					
+}) //pattern close
+  
+} 

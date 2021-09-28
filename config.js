634a988f9ae2a749478d7e26a3a4c95991274ba8
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-28 13:09:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,12 @@ NEOTROX - TEENUHX
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-
 module.exports = {
     VERSION: 'v8.0.0',
     CHANNEL: 'https://t.me/Bot_x_whatsapp',

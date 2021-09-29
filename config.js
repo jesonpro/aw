9aa9c11c06450a30a5f4835a62aab48621d9677d
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-29 22:22:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -70,6 +70,7 @@ module.exports = {
     BAD_KICK: process.env.BAD_KICK === undefined ? 'false' : process.env.BAD_KICK,
     ALL_LINK_BAN: process.env.ALL_LINK_BAN === undefined ? 'false' : process.env.ALL_LINK_BAN,
     BAD_KICK: process.env.BAD_KICK === undefined ? 'false' : process.env.BAD_KICK,
+    SITE: process.env.SITE === undefined ?'netfiletolink.herokuapp.com/' : process.env.SITE,
     INBO: process.env.INBO_BLOCK === undefined ? 'false' : process.env.INBO_BLOCK,
     BRANCH: 'master',
     HEROKU: {

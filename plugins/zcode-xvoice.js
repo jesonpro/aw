c2d3d591139e96fe5eec09bd8262c136c7d1c111
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-30 19:09:32 +0530

    Update and rename code-xvoice.js to zcode-xvoice.js

--- plugins/zcode-xvoice.js
+++ plugins/zcode-xvoice.js
@@ -8,46 +8,46 @@ const UP = "*👩‍🦰Uploading Mp3 please wait*"
 
 Neotro.addCommand({pattern: 'xvo1 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68907`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68907`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 Neotro.addCommand({pattern: 'xvo2 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68908`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68908`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 Neotro.addCommand({pattern: 'xvo3 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68911`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68911`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 Neotro.addCommand({pattern: 'xvo4 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68913`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68913`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 
 Neotro.addCommand({pattern: 'xvo5 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68914`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68914`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 
 Neotro.addCommand({pattern: 'xvo6 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}68917`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}68917`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));
 
 Neotro.addCommand({pattern: 'xvo7 ?(.*)', fromMe: tn, dontAddCommandList: true}, (async (message, match) => {
 await message.client.sendMessage(message.jid,UP,MessageType.text);
-var respoimage = await axios.get(`${Config.SITE}`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`${Config.ASITE}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data});
 
     }));

9b3046765ff6df382463e97e5b9b6e40592184f7
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-02 04:31:44 +0530

    Language support

--- plugins/MEDIALIST.js
+++ plugins/MEDIALIST.js
@@ -10,7 +10,7 @@ const Language = require('../language');
 const Config = require('../config');
 const Lang = Language.getString('amazone');
 let fs = Config.WORKTYPE == 'public' ? false : true
-
+const TT = 'Show super TTP PACK'
 Neotro.addCommand({pattern: 'textimg', fromMe: fs, desc: Lang.ALIST}, (async (message, match) => {
     await message.sendMessage("╭─「 👩‍🦰LOGO PACK 」\n│ \n*│💰How To Create Logo*\n│🔅Example : .holo hi\n│🔅Example : .glitch Amazone/Alexa\n│🔅Example : Command <your text>\n│\n│ • holo\n│ • glitch\n│ • s3d\n│ • nlogo\n│ • blackpink\n│ • neonlight\n│ • ninjalogo\n│ • 3dtext\n│ • buttonyt\n│ • 2ninja\n│ • drop\n│ •1917\n│ • boxed\n│ • battle\n│ • blood\n│ • pubg\n│ • phub\n│ • break\n│ • 2glitch\n│ • barlog\n│ • writingchalk\n│ • 2galaxy\n│ • paulscholes\n│ • multicolor3d\n│ • juventus\n│ • brokenglass\n│ • graffiti3\n│ • hotmetalic\n│ • lightgalaxy\n│ • nneon\n│ • cloud\n│ • zombie3d\n│ • bearlogo\n│ • birthdaycake\n│ • cake\n│ • artpapercut\n│ • 3dwood\n│ • realisticvintage\n│ • neondevil\n│ • neon2\n│ • thunder\n│ • graffiti5\n│ • snake\n│ • graffiticolor\n│ • metal\n│ •wordgreen\n│\n╰────「 ©️AMAZONE 」.\n\n");
 
@@ -31,7 +31,12 @@ Neotro.addCommand({pattern: 'newslist', fromMe: true, dontAddCommandList:true}, 
 
 }));
 
-Neotro.addCommand({pattern: 'moretxt', fromMe: false, desc: 'More text Logo commands'}, (async (message, match) => {
+Neotro.addCommand({pattern: 'moretxt', fromMe: fs, desc: 'More text Logo commands'}, (async (message, match) => {
   await message.sendMessage('*<=====👩‍🦰Amazone MORE LOGO pack===>*\n\n🐼command : .break\n🦋desc : change your text to wall breaking image.\n\n🐼command : .phub\n🦋desc : change your text into pornhub logo.\n🏷️example : .phuh BOT;X.\n\n🐼command : .blood\n🦋desc : change your text frozen blood on a glass\n\n🐼command : .1917\n🦋desc : change your text to a 1917 model image\n\n🐼command : .avengers\n🦋desc : change your text to avangers logo\n🏷️example : .avengers its;BOT X.\n\n🐼command : .boxed\n🦋desc : change your text to 3d boxed design\n\n🐼command : .window\n🦋desc : write your text on a foggy window \n\n🐼command : .skywal\n🦋desc : random sky wallpaper with given text\n\n🐼command : .holo\n🦋desc : change your text to holo design\n\n🐼command : .drop\n🦋desc : change your text into rain water drop.n\n🐼command : .flame\n🦋desc : text with fire effect.\n\n🐼command : .vtext\n🦋desc : text to video.\n\n🐼command : .ptext\n🦋desc : text to video.\n\n🐼command : .colortext\n🦋desc : text to colorfull video\n\n🐼command : .cloud\n🦋desc : text on cloud\n\n🐼command : .2ninja\n🦋desc : random ninja logo with given name.');
+  
+}));
+
+Neotro.addCommand({pattern: 'stlist', fromMe: fs, desc: 'More text Logo commands'}, (async (message, match) => {
+  await message.sendMessage('*╭─「 AMAZONE SUPER TTP 」*\n│ \n│🧚‍♂Amazone Alexa Super TTP pack)\n│ සිංහල Font Support 😍\n╰────\n╭─「 COMMANDS」\n│ \n│ • 1ttp <your Text >\n│ • 2ttp <your Text >\n│ • 3ttp <your Text >\n│ • 4ttp <your Text >\n│ • 5ttp <your Text >\n│ • 6ttp <your Text >\n│ • 7ttp <your Text >\n│ • 8ttp <your Text >\n│ • 9ttp <your Text >\n│ • 01ttp <your Text >\n│ • 02ttp <your Text >\n│ • 03ttp <your Text >\n│ • 04ttp <your Text >\n│ • 05ttp <your Text >\n╰────\n「 ©️Amazone-teenuhx」');
   
 }));

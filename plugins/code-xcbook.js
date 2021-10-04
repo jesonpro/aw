317c4130ef29f0b38bd2b5ad826130234375ccce
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-04 13:27:58 +0530

    🚀Xpanel- cartoon pdf Done

--- plugins/code-xcbook.js
+++ plugins/code-xcbook.js
@@ -23,7 +23,7 @@ Neotro.addCommand({pattern: 'x1book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59987`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1SDeta6pzdhfe3u5cBaOMILtnl_5B_hfB/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -35,7 +35,7 @@ Neotro.addCommand({pattern: 'x2book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59988`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1PmtH1DKmNG_HrzI_pco-QhSIYE5OZu9S/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -43,11 +43,11 @@ Neotro.addCommand({pattern: 'x2book ?(.*)', fromMe: td, dontAddCommandList: true
   
       }));
 
-Neotro.addCommand({pattern: 'x3book ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
+Neotro.addCommand({pattern: 'x3bookhttps://gist.github.com/jesonpro/424d427bacf6be3aef4a00b8a6fb91dc ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59991`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/19G1cGdn9rXX4KsUhnCbfcGgnZ-y9U6NC/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -59,7 +59,7 @@ Neotro.addCommand({pattern: 'x4book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59993`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1---v2ugC8kV8sIKzaTR57TFQDyJlHPcz/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -71,7 +71,7 @@ Neotro.addCommand({pattern: 'x5book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59995`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1b6Axf8OVhwAb4t2QZ8Ds_n5G1OHZzUVm/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -83,7 +83,7 @@ Neotro.addCommand({pattern: 'x6book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59997`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1b6Axf8OVhwAb4t2QZ8Ds_n5G1OHZzUVm/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -94,7 +94,7 @@ Neotro.addCommand({pattern: 'x7book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}59999`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1_LivUQkFFQmjfhCk3vZ2-fj39QBmynD4/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -105,7 +105,7 @@ Neotro.addCommand({pattern: 'x8book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60001`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/u/1/d/1tbqOhbxOLhpg34eTS4-V8hB_mUJ7fv5p/view?usp=drive_open`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -117,7 +117,7 @@ Neotro.addCommand({pattern: 'x9book ?(.*)', fromMe: td, dontAddCommandList: true
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60003`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1vPoD17BzLB1lW32l02NxetWVg1LumkmS/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -129,7 +129,7 @@ Neotro.addCommand({pattern: 'x10book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60005`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1EH_2G6a2IHdF9RZ89pSlzvWjxDwHQ_Iy/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -141,7 +141,7 @@ Neotro.addCommand({pattern: 'x11book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60009`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1eKMvEN0rXaCPtqjWEe_fhLu5Hom8Dp_Z/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -152,7 +152,7 @@ Neotro.addCommand({pattern: 'x12book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60011`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1RBNqpk7JAl4R7VmgKKaYPSmvHNP4y-_x/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -164,7 +164,7 @@ Neotro.addCommand({pattern: 'x13book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60013`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/19Fjz9LxHm2LA_hgrX5Um8-pJMaH5EsO0/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -176,7 +176,7 @@ Neotro.addCommand({pattern: 'x14book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60015`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1vDm5sli7M-rEsyiCDqIOhQUivTGrtSaW/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -188,7 +188,7 @@ Neotro.addCommand({pattern: 'x15book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60017`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1GbM1k3cDFLa_lzFokOaANpbUtvO2Go4j/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -200,7 +200,7 @@ Neotro.addCommand({pattern: 'x16book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60019`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1v5bZ0jz_LdgcpgxNkAZn79eJSeLlbwi_/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -212,7 +212,7 @@ Neotro.addCommand({pattern: 'x17book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60021`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1S-XYoNj396ftxfS-NggrxvU5Umr5M-Y6/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -224,7 +224,7 @@ Neotro.addCommand({pattern: 'x18book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60023`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/11v1UgwMzEo32Ifa5OT1Mi72wfE7dYy-w/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -235,7 +235,7 @@ Neotro.addCommand({pattern: 'x19book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60025`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1OBMbe9oVhaSXIi88v6DUOyTKdf4F8KcD/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -246,7 +246,7 @@ Neotro.addCommand({pattern: 'x20book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60027`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1VevBFkWTghxlcBIWb0ffox9OcdUGBO4t/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -258,7 +258,7 @@ Neotro.addCommand({pattern: 'x21book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}60029`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/13K--ez3OaUg1gDD4sf_DBSE_U6e4VOE2/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -270,7 +270,7 @@ Neotro.addCommand({pattern: 'x22book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}599`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1VbBPAAzg1ePcR5Dx7VFuKeLS0aEXSa3d/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -282,7 +282,7 @@ Neotro.addCommand({pattern: 'x23book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}599`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/1kbguyxDSUc8nqEmCIJwVkGebSfN5xMk-/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
@@ -293,7 +293,7 @@ Neotro.addCommand({pattern: 'x24book ?(.*)', fromMe: td, dontAddCommandList: tru
        await message.client.sendMessage(message.jid,XN_D,MessageType.text);
 
   
-      var webimage = await axios.get(`https://${Config.SITE}599`, { responseType: 'arraybuffer' })
+      var webimage = await axios.get(`https://drive.google.com/file/d/19G1cGdn9rXX4KsUhnCbfcGgnZ-y9U6NC/view`, { responseType: 'arraybuffer' })
 
       await message.client.sendMessage(message.jid,XN_UP,MessageType.text);
 
